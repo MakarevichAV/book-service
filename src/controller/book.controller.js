b6cf6d53f0f5b61f021efd5acf0aa8b315d10f2d
aleksandrmakarevich <aleksandr.makarevich@gamago.net>
2026-04-30 20:12:11 +0300

    findBookByIsbn function

--- src/controller/book.controller.js
+++ src/controller/book.controller.js
@@ -38,3 +38,20 @@ export const addBook = async (req, res) => {
         return res.status(500).send({error: e.message, message: 'Failed to add book'})
     }
 }
+export const findBookByIsbn = async (req, res) => {
+    const book = await Book.findByPk(req.params.isbn)
+    if (book) {
+        const result = {
+            isbn: book.isbn,
+            title: book.title,
+            publisher: book.publisher,
+            authors: (await book.getAuthors()).map(author => ({
+                name: author.name,
+                birth_date: author.birth_date
+            }))
+        }
+        return res.json(result)
+    } else {
+        return res.status(404).send({error: `Book with ISBN ${req.params.isbn} not found`})
+    }
+}
